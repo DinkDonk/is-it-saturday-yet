326cf2e42717285fefbe6ff68419dce54569117e
Rune Warhuus <rune@dinkdonk.no>
2022-05-12 21:22:35 +0200

    It's raining burgers

--- timer.js
+++ timer.js
@@ -29,6 +29,10 @@ class SaturdayTimer extends HTMLElement {
 
 		this.headerElement.innerText = 'Nope. Time left:';
 
+		this.itsRainingBurgers = false;
+
+		this.initBurgers();
+
 		this.update();
 
 		setInterval(this.update.bind(this), 1000);
@@ -38,16 +42,40 @@ class SaturdayTimer extends HTMLElement {
 		return ('0' + number).slice(-2);
 	}
 
+	initBurgers() {
+		const burger = document.querySelector('#burger');
+
+		for (let i = 0; i < 10; i++) {
+			const clone = burger.cloneNode(true);
+			clone.id = `burger-${i}`;
+			clone.style.position = 'fixed';
+			clone.style.top = '0';
+			clone.style.left = (window.innerWidth / 10) * i;
+			clone.style.width = `${10 + Math.random() * 10}%`;
+			clone.style.height = 'auto';
+			clone.style.animationName = 'fall';
+			clone.style.animationDuration = `${Math.random() * 10}s`;
+			clone.style.animationIterationCount = 'infinite';
+			clone.style.animationTimingFunction = 'linear';
+
+			document.body.appendChild(clone);
+		}
+	}
+
 	update() {
 		const now = new Date();
 
 		if (now.getDay() === 6) {
 			this.timerElement.innerText = 'YES!';
 			this.headerElement.style.display = 'none';
-			document.querySelector('svg').style.display = 'block';
+
+			document.body.classList.add('yes');
 
 			return;
 		}
+
+		document.body.classList.remove('yes');
+		this.headerElement.style.display = 'initial';
 
 		const timeLeft = ((6 - now.getDay()) * 86400) - ((now.getHours() * 3600) + (now.getMinutes() * 60) + now.getSeconds());
 		const d = Math.floor(timeLeft / (3600*24));
